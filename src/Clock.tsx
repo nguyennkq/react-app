d38c21670a8ad050d8af29cab60331585d60c199
nguyennkq <nguyenthega2k2@gmail.com>
2023-05-04 20:20:58 +0700

    lifecycle in class component

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,8 +1,19 @@
 import React from "react"
 
+const lists = ["BMW", "Toyota", "Honda"]
+
+const fetchApi = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(lists)
+    }, 1000)
+  })
+}
+
 export default class Clock extends React.Component<any, any> {
   constructor(props: any) {
     super(props)
+    console.log("constructor")
     // this.state = {
     //   time: new Date().toLocaleTimeString()
     // }
@@ -12,9 +23,42 @@ export default class Clock extends React.Component<any, any> {
       },
       seconds: {
         created: new Date().getSeconds()
-      }
+      },
+      lists: [],
+      darkMode: false
       // name: this.props.name
     }
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount")
+    fetchApi().then((res) => {
+      this.setState((prevState: any) => ({
+        ...prevState,
+        lists: res
+      }))
+    })
+    const seconds = document.querySelector("h3")
+    console.log(seconds)
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate")
+    // if (this.state.darkMode) {
+    //   const value = document.querySelector<HTMLInputElement>("input")
+    //   console.log("Value in Input", value?.value)
+    // }
+    if (this.state.lists.length === 0) {
+      fetchApi().then((res) =>
+        this.setState((prevState: any) => ({
+          ...prevState,
+          lists: res
+        }))
+      )
+    }
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount")
   }
 
   getTime = () => {
@@ -33,15 +77,25 @@ export default class Clock extends React.Component<any, any> {
     }
     this.setState(newState)
   }
+  toggleDarkMode = () => {
+    this.setState((prevState: any) => ({
+      ...prevState,
+      darkMode: !prevState.darkMode
+    }))
+  }
 
   render() {
-    console.log(this.props)
+    // console.log(this.props)
+    console.log(this.state)
+    console.log("render")
     return (
       <div>
         {/* <h2>It is {this.state.time}</h2> */}
         <h2>It is {this.state.time.created}.</h2>
         <h3>Seconds: {this.state.seconds.created}</h3>
         <button onClick={this.getTime}>Get Time</button>
+        <button onClick={this.toggleDarkMode}>Toggle</button>
+        {this.state.darkMode && <input value={this.state.darkMode} type="text" />}
       </div>
     )
   }
